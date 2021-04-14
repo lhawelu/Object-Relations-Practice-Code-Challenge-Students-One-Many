755865cb488cf48189de3e1a23d881b97caf7e21
Larry Hawelu <lhawelu@gmail.com>
2021-04-13 22:06:20 -0700

    completed

--- app/models/cohort.rb
+++ app/models/cohort.rb
@@ -1,3 +1,24 @@
 class Cohort < ActiveRecord::Base
-  
+  has_many :students
+
+  def add_student(name, age)
+    Student.create(name: name, age: age, cohort_id: self.id)
+  end
+
+  def average_age
+    self.students.all.average(:age).to_i
+  end
+
+  def total_students
+    self.students.all.length
+  end
+
+  def self.biggest
+    Cohort.all.sort_by {|c| c.students.size}.last
+  end
+
+  def self.sort_by_mod
+    Cohort.all.sort_by{|c| c.current_mod}
+  end
+
 end
---
+++ db/migrate/20210414035221_add_cohort_id_to_students.rb
@@ -0,0 +1,6 @@
+class AddCohortIdToStudents < ActiveRecord::Migration[5.2]
+  def change
+    add_column :students, :email, :string
+    add_column :students, :cohort_id, :integer
+  end
+end
